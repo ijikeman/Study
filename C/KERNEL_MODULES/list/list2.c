9bb3deb134113a77de26b0dcc1cf2e0c4b724e69
Keiji Uehata <keiji.ue@gmail.com>
2018-04-22 22:52:32 +0900

    add list2

---
+++ C/KERNEL_MODULES/list/list2.c
@@ -0,0 +1,56 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Keiji Uehata <keiji.ue@gmail.com>");
+MODULE_DESCRIPTION("an example of list data structure");
+
+static LIST_HEAD(mylist);
+
+struct mylist_entry {
+  struct list_head list;
+  int n;
+};
+
+static void mylist_add(int n) {
+  struct mylist_entry *e = kmalloc(sizeof(*e), GFP_KERNEL); // kmalloc()によって動的にメモリを確保する。GFP_KERNELはメモリ獲得時の条件を指定。通常はGFP_KERNEL
+  e->n = n; // 渡された値をmylist_entryのnに格納
+  list_add(&e->list, &mylist); // list_add()によって新規エントリをリストの先頭に追加
+  printk(KERN_ALERT "mylist: %d is added to the head\n", n);
+}
+
+static void mylist_show(void) {
+  struct mylist_entry *e;
+
+  printk(KERN_ALERT "mylist: show contents\n");
+
+  list_for_each_entry(e, &mylist, list) { // 全エントリにアクセス
+    printk(KERN_ALERT "%d\n", e->n); // mylistの値を表示する
+  }
+}
+
+static void mylist_del_head(void) {
+  struct mylist_entry *e = list_first_entry(&mylist, struct mylist_entry, list); // listの最初の値を取得
+  int n = e->n; // Message出力用に値を取得
+  list_del(&e->list); // 対象エントリをリストから削除
+  kfree(e); // 確保していたエントリ用のメモリを開放
+  printk(KERN_ALERT "mylist: %d is deleted from the head\n", n);
+}
+
+static int mymodule_init(void) {
+  mylist_show();
+  mylist_add(1);
+  mylist_show();
+  mylist_del_head();
+  mylist_show();
+
+  return 0;
+}
+
+static void mymodule_exit(void) {
+        /* Do nothing */
+}
+
+module_init(mymodule_init);
+module_exit(mymodule_exit);
