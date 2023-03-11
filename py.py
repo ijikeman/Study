7eea629fb06eb63082a3264d41def1a9a0df43ae
ijikeman <keiji.ue@gmail.com>
2023-03-11 08:27:13 +0000

    pttest

---
+++ py.py
@@ -0,0 +1,11 @@
+array = ('green', 'apple')
+result = 'apple' in array
+print(result)
+
+result = array.index('apple')
+print(result)
+
+print("%d %d" % (10, 20))
+
+print("Hello %s" % "John")
+print("Hello %s", "Score =", "%d" % ("John", 60))
