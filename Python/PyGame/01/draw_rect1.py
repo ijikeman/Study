3e0d37b800540b1b976eb3b08e3dbd859b3ed05f
ijikeman <keiji.ue@gmail.com>
2023-03-12 21:43:52 +0900

    wip

--- Python/PyGame/01/draw_rect1.py
+++ Python/PyGame/01/draw_rect1.py
@@ -1,38 +1,36 @@
+""" draw_rect1.py """
 import sys
 import pygame
 from pygame.locals import QUIT, Rect
 
 pygame.init()
-SURFACE = pygame.display.set_mode((400,300))
+SURFACE = pygame.display.set_mode((400, 300))
 FPSCLOCK = pygame.time.Clock()
 
 def main():
     """ main routine """
-
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-        SURFACE.fill((255,255,255))
-        pygame.draw.rect(SURFACE, (255,0,0),
-                         (10,20,100,50))
-        
-        pygame.draw.rect(SURFACE, (255,0,0),
-                         (150,10,100,30), 3)
+        SURFACE.fill((255, 255, 255))
 
-        pygame.draw.rect(SURFACE, (0,255,0),
-                         ((100, 80),(80, 50)))
-        
+        # 赤: 矩形塗りつぶし
+        pygame.draw.rect(SURFACE, (255,0,0), (10,20,100,50))
+        # 赤: 矩形太さ3
+        pygame.draw.rect(SURFACE, (255,0,0), (150,10,100,30), 3)
+        # 緑: 矩形
+        pygame.draw.rect(SURFACE, (0,255,0), ((100, 80),(80, 50)))
+        # 青: 矩形, Rectオブジェクト
         rect0 = Rect(200,60,140,80)
         pygame.draw.rect(SURFACE, (0,0,255),rect0)
-        
         rect1 = Rect((30,160),(100,50))
         pygame.draw.rect(SURFACE,(255,255,0), rect1)
 
         pygame.display.update()
         FPSCLOCK.tick(3)
 
-if __name__ == '__main_':
+if __name__ == '__main__':
     main()
